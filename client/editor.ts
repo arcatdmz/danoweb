34b6120d4da40cea245aab89c430c4b9a876ca53
Jun Kato <i@junkato.jp>
2019-05-04 23:32:20 +0900

    Refactor code; add "save" button

---
+++ client/editor.ts
@@ -0,0 +1,89 @@
+/// <reference path="node_modules/typescript/lib/lib.dom.d.ts" />
+/// <reference path="node_modules/monaco-editor/monaco.d.ts" />
+
+/**
+ * Monaco editor on Parcel.js bundler:
+ * https://github.com/Microsoft/monaco-editor-samples/tree/master/browser-esm-parcel
+ */
+
+/**
+ * Firepad with Monaco editor:
+ * https://github.com/FirebaseExtended/firepad/blob/master/examples/monaco.html
+ */
+import * as monaco__ from "monaco-editor";
+import * as monaco_ from "monaco-editor/esm/vs/editor/editor.main.js";
+const monaco = monaco_ as typeof monaco__;
+
+import * as firebase from "firebase/app";
+import "firebase/database";
+
+import Firepad from "firepad";
+
+import axios from "axios";
+
+export interface EditorOptions {
+  filePath?: string;
+}
+
+export class Editor {
+  private options: EditorOptions;
+  private firepad: any;
+
+  constructor(options: EditorOptions) {
+    this.options = options || {};
+  }
+  async initialize() {
+    window["MonacoEnvironment"] = {
+      getWorkerUrl: function(moduleId, label) {
+        if (label === "json") {
+          return "/lib/json.worker.js";
+        }
+        if (label === "css") {
+          return "/lib/css.worker.js";
+        }
+        if (label === "html") {
+          return "/lib/html.worker.js";
+        }
+        if (label === "typescript" || label === "javascript") {
+          return "/lib/ts.worker.js";
+        }
+        return "/lib/editor.worker.js";
+      }
+    };
+
+    const editor = monaco.editor.create(document.getElementById("firepad"), {
+      language: Editor.getLanguage(this.options.filePath),
+      automaticLayout: true
+    });
+
+    const res = await axios.get(this.options.filePath);
+    const firepadRef = firebase
+      .database()
+      .ref(`files/${Editor.getFirebasePath(this.options.filePath)}`);
+    this.firepad = Firepad.fromMonaco(firepadRef, editor, {
+      defaultText: res.data as string
+    });
+  }
+
+  static getLanguage(filePath: string) {
+    if (!filePath || filePath.length <= 0) return null;
+    switch (filePath.substr(filePath.lastIndexOf(".") + 1).toLowerCase()) {
+      case "json":
+        return "json";
+      case "css":
+        return "css";
+      case "html":
+        return "html";
+      case "ts":
+      case "tsx":
+      case "js":
+      case "jsx":
+        return "typescript";
+    }
+    return null;
+  }
+
+  static getFirebasePath(filePath: string) {
+    return encodeURIComponent(filePath).replace(/\./g, "%2E");
+  }
+}
--- client/index.ts
+++ client/index.ts
@@ -1,28 +1,11 @@
 /// <reference path="node_modules/typescript/lib/lib.dom.d.ts" />
-/// <reference path="node_modules/monaco-editor/monaco.d.ts" />
-
-/**
- * Monaco editor on Parcel.js bundler:
- * https://github.com/Microsoft/monaco-editor-samples/tree/master/browser-esm-parcel
- */
-
-/**
- * Firepad with Monaco editor:
- * https://github.com/FirebaseExtended/firepad/blob/master/examples/monaco.html
- */
-
-import * as monaco__ from "monaco-editor";
-import * as monaco_ from "monaco-editor/esm/vs/editor/editor.main.js";
-const monaco = monaco_ as typeof monaco__;
 
 import _dotenv from "dotenv";
 
 import * as firebase from "firebase/app";
 import "firebase/database";
 
-import Firepad from "firepad";
-
-import axios from "axios";
+import { Editor } from "./editor";
 
 firebase.initializeApp({
   apiKey: process.env.API_KEY,
@@ -36,63 +19,13 @@ firebase.initializeApp({
 
 window.onload = async function() {
   const filePath = location.pathname;
+  document.title = `${filePath} | denolop`;
 
-  window["MonacoEnvironment"] = {
-    getWorkerUrl: function(moduleId, label) {
-      if (label === "json") {
-        return "/lib/json.worker.js";
-      }
-      if (label === "css") {
-        return "/lib/css.worker.js";
-      }
-      if (label === "html") {
-        return "/lib/html.worker.js";
-      }
-      if (label === "typescript" || label === "javascript") {
-        return "/lib/ts.worker.js";
-      }
-      return "/lib/editor.worker.js";
-    }
-  };
-
-  const editor = monaco.editor.create(document.getElementById("firepad"), {
-    language: getLanguage(filePath)
-  });
-
-  axios
-    .get(filePath)
-    .then(res => {
-      const firepadRef = firebase
-        .database()
-        .ref(`files/${getFirebasePath(filePath)}`);
-      const firepad = Firepad.fromMonaco(firepadRef, editor, {
-        defaultText: res.data as string
-      });
-      console.log("firepad", firepad);
-    })
-    .catch(err => {
-      console.error(err);
-    });
-};
-
-function getLanguage(filePath: string) {
-  if (!filePath || filePath.length <= 0) return null;
-  switch (filePath.substr(filePath.lastIndexOf(".") + 1).toLowerCase()) {
-    case "json":
-      return "json";
-    case "css":
-      return "css";
-    case "html":
-      return "html";
-    case "ts":
-    case "tsx":
-    case "js":
-    case "jsx":
-      return "typescript";
+  const editor = new Editor({ filePath });
+  try {
+    await editor.initialize();
+    console.log("editor initialized successfully");
+  } catch (e) {
+    console.error("editor failed to initialize", e);
   }
-  return null;
-}
-
-function getFirebasePath(filePath: string) {
-  return encodeURIComponent(filePath).replace(/\./g, "%2E");
-}
+};
